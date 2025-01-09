4f216cac5c09a8e22c24cedad486d01a85011c4b
Hoseok Choi <alanhchoi@gmail.com>
2025-01-09 18:25:45 -0500

    fix dark styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div className="p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-xl font-bold">Hoseok Choi</h1>
         <div className="flex items-center gap-2 flex-wrap">
+          <Link className="hover:underline hover:underline-offset-4" href="/portfolio">Portfolio</Link>
+          <hr className="border" />
           <a className="hover:underline hover:underline-offset-4" target="_blank" href="https://www.linkedin.com/in/hoseokchoi/">LinkedIn</a>
           <a className="hover:underline hover:underline-offset-4" target="_blank" href="https://www.instagram.com/hoseok.choi/">Instagram</a>
           <a className="hover:underline hover:underline-offset-4" target="_blank" href="https://www.youtube.com/@iamhoseokchoi">YouTube</a>
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -11,15 +11,15 @@ import tacoThumbnail from "../../public/taco-thumb.png";
 
 export default function Portfolio() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)] grid place-items-center min-h-screen bg-zinc-200">
-      <main className="max-w-screen-lg bg-white min-h-full pb-20">
+    <div className="font-[family-name:var(--font-geist-sans)] grid place-items-center min-h-screen bg-zinc-200 dark:bg-zinc-950">
+      <main className="max-w-screen-lg bg-white dark:bg-zinc-900 min-h-full pb-20">
         <div className="p-8 sm:p-20 sm:pb-8 flex flex-col gap-8">
           <h1 className="text-xl font-bold">
             <Link className="hover:underline hover:underline-offset-4" href="/">Hoseok Choi</Link>
           </h1>
           <h2 className="text-lg font-bold">Editorial Design</h2>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-3 w-full gap-px border-y lg:px-20 lg:border-y-0 lg:gap-px">
+        <div className="grid grid-cols-1 sm:grid-cols-3 w-full gap-px border-y lg:px-20 lg:border-y-0 dark:border-transparent lg:gap-px">
           <a href="/magazine.pdf" target="_blank" className="filter hover:brightness-75"><Image src={magazineThumb} alt="Magazine" /></a>
           <a href="/catalog.pdf" target="_blank" className="filter hover:brightness-75"><Image src={catalogThumb} alt="Catalog" /></a>
           <a href="/textbook.pdf" target="_blank" className="filter hover:brightness-75"><Image src={textbookThumb} alt="Textbook" /></a>
@@ -27,7 +27,7 @@ export default function Portfolio() {
         <div className="p-8 sm:p-20 sm:pb-8 flex flex-col gap-8">
           <h2 className="text-lg font-bold">Digital Illustrations</h2>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-3 w-full gap-px border-y lg:px-20 lg:border-y-0 lg:gap-px">
+        <div className="grid grid-cols-1 sm:grid-cols-3 w-full gap-px border-y lg:px-20 lg:border-y-0 dark:border-transparent lg:gap-px">
           <a href="/itaewon.png" target="_blank" className="filter hover:brightness-75"><Image src={itaewonThumbnail} alt="Itaewon, Seoul" /></a>
           <a href="/guam.jpg" target="_blank" className="filter hover:brightness-75"><Image src={guamThumbnail} alt="Guam" /></a>
           <a href="/taco.jpg" target="_blank" className="filter hover:brightness-75"><Image src={tacoThumbnail} alt="Taco" /></a>
@@ -37,7 +37,7 @@ export default function Portfolio() {
         <div className="p-8 sm:p-20 sm:pb-8 flex flex-col gap-8">
           <h2 className="text-lg font-bold">Motion Graphics</h2>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-3 w-full gap-px border-y lg:px-20 lg:border-y-0 lg:gap-px">
+        <div className="grid grid-cols-1 sm:grid-cols-3 w-full gap-px border-y lg:px-20 lg:border-y-0 dark:border-transparent lg:gap-px">
           <a href="https://www.youtube.com/watch?v=S9N1ABZ9Wl8" target="_blank" className="filter hover:brightness-75">
             <video preload="none" aria-label="Video player" className="w-full aspect-square object-cover" autoPlay muted loop>
               <source src="/motion-thumb.mp4" type="video/mp4" />
