95eb9e4d8c78afe98e1bd445d9e03ac1feaef18d
Hoseok Choi <alanhchoi@gmail.com>
2025-01-09 17:23:41 -0500

    add illustration section

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 import magazine from "../../public/magazine.png";
 import catalog from "../../public/catalog.png";
 import textbook from "../../public/textbook.png";
+import guamThumbnail from "../../public/guam-thumb.png";
+import itaewonThumbnail from "../../public/itaewon-thumb.png";
+import seoulDayThumbnail from "../../public/seoul-day-thumb.png";
+import seoulNightThumbnail from "../../public/seoul-night-thumb.png";
+import tacoThumbnail from "../../public/taco-thumb.png";
 
 export default function Portfolio() {
   return (
@@ -18,15 +23,17 @@ export default function Portfolio() {
           <a href="/catalog.pdf" target="_blank" className="filter hover:brightness-75"><Image src={catalog} alt="Catalog" /></a>
           <a href="/textbook.pdf" target="_blank" className="filter hover:brightness-75"><Image src={textbook} alt="Textbook" /></a>
         </div>
+        <div className="p-8 sm:p-20 sm:pb-8 flex flex-col gap-8">
+          <h2 className="text-lg font-bold">Digital Illustrations</h2>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-3 w-full gap-px border-y lg:px-20 lg:border-y-0 lg:gap-px">
+          <a href="/itaewon.png" target="_blank" className="filter hover:brightness-75"><Image src={itaewonThumbnail} alt="Itaewon, Seoul" /></a>
+          <a href="/guam.jpg" target="_blank" className="filter hover:brightness-75"><Image src={guamThumbnail} alt="Guam" /></a>
+          <a href="/taco.jpg" target="_blank" className="filter hover:brightness-75"><Image src={tacoThumbnail} alt="Taco" /></a>
+          <a href="/seoul-night.png" target="_blank" className="filter hover:brightness-75"><Image src={seoulNightThumbnail} alt="Night in Seoul" /></a>
+          <a href="/seoul-day.png" target="_blank" className="filter hover:brightness-75"><Image src={seoulDayThumbnail} alt="Day in Seoul" /></a>
+        </div>
       </main>
     </div>
   );
-}
-
-Portfolio.getLayout = function getLayout(page) {
-  return (
-    <body
-        className="bg-zinc-100"
-      >{page}</body>
-  )
 }
