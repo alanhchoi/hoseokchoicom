36b67a3588aede5da1afefd787c4a878fda3cdb4
Hoseok Choi <alanhchoi@gmail.com>
2025-01-09 21:46:26 -0500

    track links

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-xl font-bold">Hoseok Choi</h1>
         <div className="flex items-center gap-2 flex-wrap">
-          <Link className="hover:underline hover:underline-offset-4" href="/portfolio">Portfolio</Link>
+          <Link className="hover:underline hover:underline-offset-4" href="/portfolio"  data-umami-event="Portfolio link">Portfolio</Link>
           <hr className="border" />
-          <a className="hover:underline hover:underline-offset-4" target="_blank" href="https://www.linkedin.com/in/hoseokchoi/">LinkedIn</a>
-          <a className="hover:underline hover:underline-offset-4" target="_blank" href="https://www.instagram.com/hoseok.choi/">Instagram</a>
-          <a className="hover:underline hover:underline-offset-4" target="_blank" href="https://www.youtube.com/@iamhoseokchoi">YouTube</a>
+          <a className="hover:underline hover:underline-offset-4" target="_blank" href="https://www.linkedin.com/in/hoseokchoi/" data-umami-event="LinkedIn link">LinkedIn</a>
+          <a className="hover:underline hover:underline-offset-4" target="_blank" href="https://www.instagram.com/hoseok.choi/" data-umami-event="Instagram link">Instagram</a>
+          <a className="hover:underline hover:underline-offset-4" target="_blank" href="https://www.youtube.com/@iamhoseokchoi" data-umami-event="YouTube link">YouTube</a>
         </div>
       </main>
     </div>
