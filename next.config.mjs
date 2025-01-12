0e2cda13ead9794c0b1acc47c35f0b64d4992993
Hoseok Choi <alanhchoi@gmail.com>
2025-01-12 17:51:00 -0500

    add redirects from books-i-read to blog

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/books-i-read",
+        destination: "/blog",
+        permanent: true,
+      },
+      {
+        source: "/books-i-read/:slug",
+        destination: "/blog/:slug",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 if (process.env.NODE_ENV === "development") {
